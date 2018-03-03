c027b5ef9a4f8362a09f9e7d172beaa83518bbaa
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-03-03 14:10:21 +0300

    (+) package "Microsoft.AspNet.Identity.EntityFramework", "AppUserModel" entity

---
+++ IdentyOWIN/Models/AppUserModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentyOWIN.Models
+{
+    public class AppUser : IdentityUser
+    {
+
+    } // end class AppUser
+
+
+
+
+
+} // end namespace
