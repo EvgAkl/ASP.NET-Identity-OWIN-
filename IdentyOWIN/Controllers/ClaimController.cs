4249cfef7bb026de333839051c3e26b991e09d88
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-04-11 16:42:19 +0300

    (+) "Claims" controller and action method "Index" in it

---
+++ IdentyOWIN/Controllers/ClaimController.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentyOWIN.Controllers
+{
+    public class ClaimController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            ClaimsIdentity ident = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (ident == null)
+            {
+                return View("Error", new string[] { "Нет доступных требований" });
+            }
+            else
+            {
+                return View(ident.Claims);
+            }
+
+        } // end Index()
+
+
+
+    } // end controller
+
+} // end namespace
