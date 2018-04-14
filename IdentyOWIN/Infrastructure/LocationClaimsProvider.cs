4eee18d0dc8fc2f1918e0373178f1133229a551c
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-04-14 14:37:16 +0300

    (+) LocationClaimsProvider class and GetClaims method in it

---
+++ IdentyOWIN/Infrastructure/LocationClaimsProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.Collections.Generic;
+
+namespace IdentyOWIN.Infrastructure
+{
+    public class LocationClaimsProvider
+    {
+        private static Claim CreateClaim(string type, string value)
+        {
+            return new Claim(type, value, ClaimValueTypes.String, "RemoteClaims");
+        } // end CreateClaim
+
+        public static IEnumerable<Claim> GetClaims(ClaimsIdentity user)
+        {
+            List<Claim> claims = new List<Claim>();
+
+            if (user.Name.ToLower() == "елена")
+            {
+                claims.Add(CreateClaim(ClaimTypes.PostalCode, "DC 20500"));
+                claims.Add(CreateClaim(ClaimTypes.StateOrProvince, "DC"));
+            }
+            else
+            {
+                claims.Add(CreateClaim(ClaimTypes.PostalCode, "NY 10036"));
+                claims.Add(CreateClaim(ClaimTypes.StateOrProvince, "NY"));
+            }
+            return claims;
+        } // end GetClaims()
+
+    } // end class
+
+} // end namespace
